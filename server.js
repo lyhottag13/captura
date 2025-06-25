c271c09db61d3de4bdad6dd7452854fe0446c75d
lyhottag13 <leonardo@promoevt.com>
2025-06-24 22:40:27 -0700

    Updated Capabilities

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 const mysql = require('mysql2/promise');
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 80;
+
+let pool;
+
+createConnection();
 
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send(`
-        <form method="Post" action="/submit">
+        <h1 id="header"></h1>
+        <form method="post" action="/submit">
             <input name="username" placeholder="Enter your name" />
             <button type="submit">Send</button>
         </form>
+        <script>
+            const header = document.getElementById("header");    
+            setInterval(() => {
+                header.innerHTML = Date().;
+            }, 1000);
+        </script>
         `);
 });
 app.get('/cool', (req, res) => {
-    res.send('<h1>Hi!</h1>');
+    res.send(`
+        <h1>Hi!</h1>
+        `);
 });
 
 app.listen(PORT, () => {
@@ -22,32 +35,22 @@ app.listen(PORT, () => {
 });
 
 app.post('/submit', (req, res) => {
-    res.send(`Thank you! I have your info now. Your name is ${req.body.username}`);
+    res.send(`Thank you! I have your info now. Your name is ${req.body.username}
+        <br>
+        <a href="/">Return</a>
+        `);
+    insert(req.body.username);
 });
-async function main() {
-    const pool = mysql.createPool({
+function createConnection() {
+    pool = mysql.createPool({
         host: 'localhost',
         user: 'root',
         password: 'Leo!2025',
         database: 'sys',
     });
-
-    try {
-        const [results] = await pool.query('SELECT * FROM breville2');
-        results.forEach((row) => {
-            let rowString = "(";
-            for (const item in row) {
-                try {
-                    rowString = rowString + ", " + row[item].substring(0, 2);
-                } catch (e) {
-
-                }
-            };
-            console.log(rowString + ")");
-        })
-    } catch (e) {
-        console.log(e);
-    }
-
 }
-main();
+
+async function insert(name) {
+    pool.execute(`INSERT INTO names (name) VALUES ('${name}')`);
+    console.log(name);
+}
